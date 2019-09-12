cfc5d320c3e55b5d16acd4f01792fe0e27796c29
Neverkas <emmanuel.lazarte.011@gmail.com>
2019-09-12 19:14:11 -0300

    Se agregaron excepciones y mas pruebas unitarias

--- src/iteradores/DosArraysIterator.java
+++ src/iteradores/DosArraysIterator.java
@@ -1,28 +1,63 @@
 package iteradores;
-public class DosArraysIterator{
-	int [] array1 = {};
-	int numero;
-	int contador = 0;
-	
-	//public static void main(String[] args) {	
-	//}
+
+import java.util.Iterator;
+
+public class DosArraysIterator implements Iterator<Object>{
+	int [] array1, array2 = {};
+	int elemento;	
+	int indiceArray1, indiceArray2 = 0;
 	
 	public DosArraysIterator(int [] arr1, int [] arr2) {
 		array1 = arr1;
-		//array2 = arr2;
-	}
-	
+		array2 = arr2;
+	}	
 
+	@Override // sobreescribimos la función original por una propia, con fines prácticos
 	public boolean hasNext() {		
-		while(contador < array1.length) {			
-			
-			contador +=1;
+		// si los arrays son nulos (no apuntan a ninguna dirección en memoria ni ocupan espacio en memoria, no tienen datos)
+		if(this.array1 == null && this.array2 == null) {
+			// no hay necesidad que sigamos evaluando, damos por finalizada la ejecución
+			return false;
 		}
 		
-		return false;
+		// verificamos si el indice o posicion del elemento actual al que apuntan 
+		// cada array no supere la longitud de ellos
+		// sino se cumple no hay elementos que le sigan por ser los ultimos
+		return this.indiceArray1 < array1.length && this.indiceArray2 < array2.length;
 	}
 
-	public int next() {
-		return array1[contador];
+	@Override // sobreescribimos la función original por una propia, con fines prácticos
+	public Object next() {
+		int indiceActualArray1, indiceActualArray2;		
+		// si el array es nulo (no apunta a ninguna dirección en memoria ni ocupa espacio en memoria, no tiene datos)
+		if(this.array1 == null || this.array2 == null) {
+			// devolvemos una excepción porque no podriamos acceder a ningun elemento
+			throw new NullPointerException();
+		}
+		// en caso que el array esté tenga cero (vacío), uno o más elementos
+		try {			
+			// guardamos la posicion actual antes de apuntar al siguiente elemento de cada array
+			indiceActualArray1 = this.indiceArray1;
+			indiceActualArray2 = this.indiceArray2;
+			// si el indice del elemento no supera la longitud del array
+			if(this.indiceArray1 < this.array1.length) {
+				// hago que el array apunte al siguiente elemento
+				this.indiceArray1 += 1;				
+				// guardo el valor que voy a devolver
+				this.elemento = array1[indiceActualArray1];
+			}
+			// si el array1 no tiene mas elementos, evaluo el array2
+			// utilizamos el mismo criterio que con el array1
+			else if(this.indiceArray2 < this.array2.length) {
+				this.indiceArray2 += 1;				
+				
+				this.elemento = array2[indiceActualArray2];
+			}
+			
+			return this.elemento;
+			
+		} catch (Exception e) {
+			throw new ArrayIndexOutOfBoundsException("El elemento no existe o el indice supera la longitud del array en cuestión");
+		}					
 	}
 }
--- src/iteradores/DosArraysIteratorTest.java
+++ src/iteradores/DosArraysIteratorTest.java
@@ -4,29 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class DosArraysIteratorTest {
-/*
- * precondicion->accion->postcondicion
- * dosArrayVacios
- * arrayVacio + [1,2]
- * [1,2] + arrayVacio
- * dosNull
- * null + [1,2]
- * [1,2] + null
- * [1,2] + [3,4]
- * null + vacio
- * vacio + null
- */
 	@Test
 	void DosArraysVacios() {		
-		int [] array1 = {1,2};
-		int [] array2 = {3,4};
+		int[] array1 = {};
+		int[] array2 = {};
 		
 		DosArraysIterator it = new DosArraysIterator(array1, array2);
 		
-		while(it.hasNext()) {
-			int i = it.next();
-			System.out.println(i);			
-		}
+		assertFalse(it.hasNext());
+	}
+	
+	@Test
+	void soloPrimerArrayVacio() {
+		int[] array1 = {};
+		int[] array2 = {1, 3};
+		
+		DosArraysIterator it = new DosArraysIterator(array1, array2);
+
+		assertFalse(it.hasNext()); // 1er array -> no tiene elementos, está vacío		
+		assertEquals(1, it.next()); // 2do array -> 1er elemento debe ser 1
+		assertTrue(it.hasNext()); // 2do array -> no deberia haber mas elementos
+		/*		
+		
+		assertEquals(3, it.next()); // 2do array -> 2do elemento debe ser 3
+		*/
 	}
 
 }
